535f268fce5175e95968c2dbff960d2600c54c96
jfk86 <habiburrahman86@gmail.com>
2025-09-17 10:15:45 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,18 +2,16 @@ const path = require('path');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  distDir: process.env.NEXT_DIST_DIR || '.next',
-  output: process.env.NEXT_OUTPUT_MODE,
-  experimental: {
-    outputFileTracingRoot: path.join(__dirname, '../'),
-  },
   eslint: {
     ignoreDuringBuilds: true,
   },
   typescript: {
     ignoreBuildErrors: false,
   },
-  images: { unoptimized: true },
+  images: { 
+    unoptimized: true 
+  },
+  // Remove experimental options that might cause issues
 };
 
 module.exports = nextConfig;
